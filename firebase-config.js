dbd550951e8b768b7d9dcd25b59b0667c3786117
Yilizire Yiliyaer <yyil0178@uni.sydney.edu.au>
2026-09-03 20:07:04 +1000

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,9 @@
 window.MY_ROSTER_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyC_XFG-01NtK6dvTGSGr23wofHbPq7eaRY",
+  authDomain: "my-weekly-roster.firebaseapp.com",
+  projectId: "my-weekly-roster",
+  storageBucket: "my-weekly-roster.firebasestorage.app",
+  messagingSenderId: "767717286436",
+  appId: "1:767717286436:web:78a74f537f6fdca5486eb1",
+  measurementId: "G-Z0HRQFE0VN"
 };
